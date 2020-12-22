4c6a41d38acdd6e68f76ed6a86f56e4479cd94ce
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-12-22 16:34:19 +0900

    initialiized react project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>
+                    Hello World
+                </h3>
+            </div>
+        );
+    }
+}
+
+export default App; 
